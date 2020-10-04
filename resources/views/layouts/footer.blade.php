ac197e7278cf0430efda86d657dab7bbf4279f5e
Damien <damien@damienvalade.fr>
2020-10-05 00:08:26 +0200

    adjust footer
    adjust footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+{# **************** #}
+{# **** Footer **** #}
+{# **************** #}
+
+<footer class="footer bg-dark pt-3 pb-3">
+    <ul class="container d-flex justify-content-center mb-0">
+        <li class="ml-3 mr-3"><a href="https://github.com/damienvalade"><i class="fab fa-github"></i></a></li>
+        <li class="ml-3 mr-3"><a href="https://www.linkedin.com/in/damien-valade/"><i class="fab fa-linkedin"></i></a></li>
+        <li class="ml-3 mr-3"><a href="https://twitter.com/valade_damien"><i class="fab fa-twitter"></i></a></li>
+        <li class="ml-3 mr-3"><a href="https://codepen.io/damienvalade/#"><i class="fab fa-codepen"></i></a></li>
+        <li class="ml-3 mr-3"><a href="https://stackoverflow.com/story/damienvalade"> <i class="fab fa-stack-overflow"></i></a></li>
+    </ul>
+</footer>
