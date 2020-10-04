6e4e6464ddf277853201097457ff28751317b02d
Damien <damien@damienvalade.fr>
2020-10-05 01:00:18 +0200

    add data to home view
    add data to home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Diploma;
+use App\Models\OcCertificate;
+use App\Models\Project;
+use App\Models\Skills;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    public $skills;
+    public $diploma;
+    public $oc_certificate;
+    public $project;
+
+    public function __construct()
+    {
+        $this->skills = Skills::all();
+        $this->diploma = Diploma::all();
+        $this->oc_certificate = OcCertificate::all();
+        $this->project = Project::all();
+    }
+
     public function index()
     {
-        return view('publicside.home');
+        return view('publicside.home', [
+            'skills' => $this->skills,
+            'diploma' => $this->diploma,
+            'oc_certificate' => $this->oc_certificate,
+            'project' => $this->project,
+        ]);
     }
 }
