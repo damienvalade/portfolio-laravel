48205c871ae638e2177bd9848d80e8125d7f7678
Damien <damien@damienvalade.fr>
2020-10-05 03:18:23 +0200

    rename tables
    rename tables

--- database/migrations/2020_10_02_120801_diplomas_table.php
+++ database/migrations/2020_10_02_120801_diplomas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class DiplomaTable extends Migration
+class DiplomasTable extends Migration
 {
     /**
      * Run the migrations.
